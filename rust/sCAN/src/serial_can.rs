3fd01fd1b23712ab986c0c41e637eb7edd7588be
Jacob Chisholm <Jacobchisholm1010@gmail.com>
2024-04-10 19:54:19 -0400

    Update serial_can.rs

--- rust/sCAN/src/serial_can.rs
+++ rust/sCAN/src/serial_can.rs
@@ -1,5 +1,8 @@
-#![allow(unused)]
 use serialport;
+use std::time::Duration;
+use std::io;
+use std::thread;
+use std::sync::mpsc;
 
 const BUFFER_LEN : usize = 64;
 
@@ -11,7 +14,7 @@ pub struct CANmsg{
 }
 
 impl CANmsg {
-    pub fn default() -> CANmsg {
+    pub fn default() -> Self {
         CANmsg {
             id : 0,
             data : [0; 8],
@@ -25,17 +28,49 @@ enum State {
 }
 
 pub struct SerialCAN{
-    serial_port : Box<dyn serialport::SerialPort>,
-    state : State,
-    buffer: [CANmsg; BUFFER_LEN],
+    receiver    : mpsc::Receiver<CANmsg>,
+    transmitter : mpsc::Sender<CANmsg>,
+
 }
 
 impl SerialCAN {
     pub fn new(port_name: String, baud : u32) -> Self{
-        SerialCAN{
-            serial_port : serialport::new(port_name.as_str(), baud).open().expect("Failed to open Serial Port"),
-            state : State::ID,
-            buffer : [CANmsg::default(); BUFFER_LEN],
+        let (ttx, trx) = mpsc::channel();
+        let (rtx, rrx) = mpsc::channel();
+        
+        let serial_port = serialport::new(port_name.as_str(), baud).open().expect("Failed to open Serial Port");
+        thread::spawn(move ||{
+            let mut serial_port = serial_port;
+            let _ = serial_port.set_timeout(Duration::from_millis(1));
+            let mut trx = trx;
+            let mut rtx = rtx;
+            loop{
+                Self::rx_tick(&mut serial_port, &mut rtx);
+                Self::tx_tick(&mut serial_port, &mut trx);
+            }
+        });
+
+        SerialCAN {
+            receiver: rrx,
+            transmitter : ttx,
         }
+    }
+
+    fn rx_tick(port : & mut Box<dyn serialport::SerialPort>, writer: &mut mpsc::Sender<CANmsg>){
+        let mut rx_buffer: [u8; 64] = [0; 64];
+        match port.read(&mut rx_buffer){
+            Ok(bytes) => {
+                if bytes != 0 {
+                }
+            },
+            Err(_) => {}
+        }
+        Self::crc_check(&CANmsg::default());
+    }
+
+    fn tx_tick(port : & mut Box<dyn serialport::SerialPort>, reader: &mut mpsc::Receiver<CANmsg>){}
+
+    fn crc_check(msg: &CANmsg) -> bool {
+        return false;
     }
 }
