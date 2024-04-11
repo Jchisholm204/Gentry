266bcbc32f66b8deb39915114a7bba32625f0879
Jacob Chisholm <Jacobchisholm1010@gmail.com>
2024-04-10 21:38:47 -0400

    Update serial_can.rs

--- rust/sCAN/src/serial_can.rs
+++ rust/sCAN/src/serial_can.rs
@@ -58,24 +58,32 @@ impl SerialCAN {
 
     fn tick(port : & mut Box<dyn serialport::SerialPort>, writer: &mut mpsc::Sender<CANmsg>, reader: &mut mpsc::Receiver<CANmsg>){
         let mut state : State = State::ID;
-        let mut buffer : [CANmsg; BUFFER_LEN];
-        let mut read_index : usize = 0;
-        let mut write_index: usize = 0;
         let mut temp_msg : CANmsg = CANmsg::default();
+        let mut internal : usize = 0;
 
         loop{
             // Begin Receiver
-            let mut rx_buffer: [u8; 64] = [0; 64];
-            match port.read(&mut rx_buffer){
-                Ok(mut bytes) => {
-                    while bytes > 0 {
+            let mut byte_buffer: [u8; 64] = [0; 64];
+            match port.read(&mut byte_buffer){
+                Ok(bytes) => {
+                    for i in 0..bytes {
                         match state {
-                            State::ID => {}
+                            State::ID => {
+                                temp_msg.id |= (byte_buffer[i] as u32) << 8*internal;
+                                internal += 1;
+                                if internal >= 4 {
+                                    internal = 0;
+                                    state = State::LEN;
+                                }
+                            }
                             State::LEN => {}
                             State::Data => {}
-                            State::CRC => {}
+                            State::CRC => {
+                                if Self::crc_check(&temp_msg) {
+                                    let _ = writer.send(temp_msg);
+                                }
+                            }
                         }
-                        bytes -= 1;
                     }
                 },
                 Err(_) => {}
