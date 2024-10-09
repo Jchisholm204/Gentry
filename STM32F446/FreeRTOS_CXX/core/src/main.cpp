f633380a44e5960ef42de9fa8f12df414ab4e45a
Jacob Chisholm <Jacobchisholm1010@gmail.com>
2024-10-08 21:25:27 -0400

    eh.. idk if worth

--- STM32F446/FreeRTOS_CXX/core/src/main.cpp
+++ STM32F446/FreeRTOS_CXX/core/src/main.cpp
@@ -29,10 +29,25 @@ extern "C" void vTestTask(void * pvParams){
     (void)pvParams;
     const static char* str = "Hello World from Serial 2\n";
     gpio_set_mode(PIN('B', 0), GPIO_MODE_OUTPUT);
+    // hal_uart_init(USART2, 9600, PIN('A', 2), PIN('A', 3));
     for(;;){
         gpio_write(PIN('B', 0), !gpio_read_odr(PIN('B', 0)));
-        // Serial2.write((uint8_t*)str, strlen(str));
+        // hal_uart_write_buf(USART2, (char*)str, strlen(str));
+        Serial2.write((uint8_t*)str, strlen(str));
         // sleep for 1000 ms
+        vTaskDelay(1000);
+    }
+}
+
+extern "C" void vUART_FeedBack(void * pvParams){
+    (void)pvParams;
+    StreamBufferHandle_t xStreamBuffer = xStreamBufferCreate(100, 1);
+    // Serial2.attach(xStreamBuffer);
+    for(;;){
+        char buffer[] = "Task2\n";
+        // int bytes = xStreamBufferReceive(xStreamBuffer, (void*)buffer, 100, portMAX_DELAY);
+        // printf("Serial 2 got: %s", buffer);
+        Serial2.write((uint8_t*)buffer, 7);
         vTaskDelay(1000);
     }
 }
@@ -40,7 +55,7 @@ extern "C" void vTestTask(void * pvParams){
 // Initialize all system Interfaces
 void Init(void){
     
-    Serial2.setup(9600, PIN('D', 6), PIN('D', 5));
+    Serial2.setup(9600, PIN('A', 3), PIN('A', 2));
 }
 
 
@@ -49,6 +64,7 @@ extern "C" int main(void){
     Init();
     
     xTaskCreate(vTestTask, "TestTask", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
+    xTaskCreate(vUART_FeedBack, "S2 Echo", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
 
     // Start Scheduler
     vTaskStartScheduler();
