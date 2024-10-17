6088f4e3a4cfa3310ba0c27fba0232179c8e63ba
Jacob Chisholm <Jacobchisholm1010@gmail.com>
2024-10-17 19:25:30 -0400

    still not working

--- STM32F446/FreeRTOS_C/core/src/main.c
+++ STM32F446/FreeRTOS_C/core/src/main.c
@@ -33,7 +33,7 @@ void vTestTask(void * pvParams){
     for(;;){
         gpio_write(PIN('B', 0), !gpio_read_odr(PIN('B', 0)));
         // serial_write(&Serial2, str, strlen(str), 100);
-        printf("Hello from Serial 2\n");
+        // printf("Hello from Serial 2\n");
         // hal_uart_write_buf(USART2, (char*)str, strlen(str));
         // Serial2.write((uint8_t*)str, strlen(str));
         // sleep for 1000 ms
@@ -101,111 +101,99 @@ void vUART_FeedBack(void * pvParams){
 // // vTaskDelay(portMAX_DELAY);
 // }
 
-volatile bool session_active = false;
-void vUSB_tsk(void * pvParams){
-    (void)pvParams;
-    printf("Initializing USB\n");
-
-    // Enable USB in the RCC (USB OTG FS clock enable)
-    SET_BIT(RCC->AHB2ENR, RCC_AHB2ENR_OTGFSEN);
-
-    // Setup the GPIO pins for USB DM and DP with alternate function and proper speed
-    // gpio_set_mode(PIN_USB_DM, GPIO_MODE_AF);
-    // gpio_set_af(PIN_USB_DM, GPIO_AF_USB);
-    // gpio_set_speed(PIN_USB_DM, GPIO_SPEED_HS);
-
-    // gpio_set_mode(PIN_USB_DP, GPIO_MODE_AF);
-    // gpio_pull(PIN_USB_DP, GPIO_PULLUP); // Enable the pull-up for USB DP
-    // gpio_set_af(PIN_USB_DP, GPIO_AF_USB);
-    // gpio_set_speed(PIN_USB_DP, GPIO_SPEED_HS);
-
-    GPIOA->MODER &= ~(0x3 << (PINNO(PIN_USB_DM)*2));
-    GPIOA->MODER |= (0b10 << (PINNO(PIN_USB_DM)*2));
-
-    // GPIOA->OSPEEDR &= ~(0x3 << (PINNO(PIN_USB_DM)*2));
-    GPIOA->OSPEEDR |= (0x3 << (PINNO(PIN_USB_DM)*2));
-
-    GPIOA->AFR[1] &= ~(0b1111 << ((PINNO(PIN_USB_DM) & 7)*4));
-    GPIOA->AFR[1] |= (0b1010 << ((PINNO(PIN_USB_DM) & 7)*4));
-
-    GPIOA->MODER &= (uint32_t)~(uint32_t)((uint32_t)0x3UL << (uint32_t)(PINNO(PIN_USB_DP)*2UL));
-    GPIOA->MODER |= (0b10 << (PINNO(PIN_USB_DP)*2));
-
-    // GPIOA->OSPEEDR &= ~(0x3 << (PINNO(PIN_USB_DM)*2));
-    GPIOA->OSPEEDR |= (0x3 << (PINNO(PIN_USB_DP)*2));
-
-    GPIOA->AFR[1] &= ~(0b1111 << ((PINNO(PIN_USB_DP) & 7)*4));
-    GPIOA->AFR[1] |= (0b1010 << ((PINNO(PIN_USB_DP) & 7)*4));
-
-    // Core Initialization
-    // Reset GAHBCFG to its default value
-    USB_OTG_FS->GAHBCFG = 0x00000000;
-
-    // Enable global interrupts in the AHB configuration register
-    SET_BIT(USB_OTG_FS->GAHBCFG, USB_OTG_GAHBCFG_GINT);
-
-    // OTG USB Configuration Register - USB turnaround time
-    USB_OTG_FS->GUSBCFG = 0x00001440;
-    USB_OTG_FS->GUSBCFG |= 0x6 << USB_OTG_GUSBCFG_TRDT_Pos;  // Set TRDT to the recommended value for FS
-
-    // Finalize Interrupts in Interrupt Mask Reg
-    SET_BIT(USB_OTG_FS->GINTMSK, USB_OTG_GINTMSK_OTGINT);    // OTG interrupts
-    SET_BIT(USB_OTG_FS->GINTMSK, USB_OTG_GINTMSK_MMISM);     // Mode mismatch interrupt
-
-    // Device Mode Check (CMOD = 0 for device mode)
-    bool devon = USB_OTG_FS->GINTSTS & USB_OTG_GINTSTS_CMOD;
-    printf("USB Device Mode? %d\n", !devon);
-
-    // USB Device Initialization
-    USB_OTG_DeviceTypeDef *usbdev = ((USB_OTG_DeviceTypeDef*)USB_OTG_DEVICE_BASE);
-
-    // DCFG: Device Configuration Register
-    usbdev->DCFG = 0x02200000;  // Full-speed device (FS)
-    usbdev->DCFG |= 0x3 << USB_OTG_DCFG_PFIVL_Pos;  // Set periodic frame interval to 95%
-    usbdev->DCFG &= ~(USB_OTG_DCFG_DSPD);  // Clear previous speed config
-    usbdev->DCFG |= 0x3 << USB_OTG_DCFG_DSPD_Pos;   // Set device speed to Full-Speed
-
-    // Clear the SDIS bit (Soft Disconnect), enabling connection to the host
-    usbdev->DCTL &= ~(USB_OTG_DCTL_SDIS);
-
-    // Enable the USB pull-up on D+ (to signal the host)
-    usbdev->DCTL |= USB_OTG_DCTL_RWUSIG;  // Remote wakeup signaling, forcing D+ pull-up
-
-    // Enable necessary USB interrupts in GINTMSK
-    SET_BIT(USB_OTG_FS->GINTMSK, USB_OTG_GINTMSK_USBSUSPM);  // USB Suspend
-    SET_BIT(USB_OTG_FS->GINTMSK, USB_OTG_GINTMSK_RXFLVLM);   // RX FIFO non-empty interrupt
-    SET_BIT(USB_OTG_FS->GINTMSK, USB_OTG_GINTMSK_IEPINT);   // IN endpoint interrupt
-    SET_BIT(USB_OTG_FS->GINTMSK, USB_OTG_GINTMSK_OEPINT);   // OUT endpoint interrupt
-    SET_BIT(USB_OTG_FS->GINTMSK, USB_OTG_GINTMSK_SOFM);      // Start of Frame interrupt
-    SET_BIT(USB_OTG_FS->GINTMSK, USB_OTG_GINTMSK_ENUMDNEM);  // Enumeration done interrupt
-    // Enable Session Request interrupt
-    SET_BIT(USB_OTG_FS->GINTMSK, USB_OTG_GINTMSK_SRQIM);
-
-    // Enable the USB OTG FS interrupt in NVIC
+void USB_Init_Core(void){
+    // Enable USB FS interrupts
     NVIC_SetPriority(OTG_FS_IRQn, NVIC_Priority_MIN-2);
     NVIC_EnableIRQ(OTG_FS_IRQn);
 
-    gpio_set_mode(PIN_USB_VBUS, GPIO_MODE_INPUT);
+    // Enable USB clock
+    SET_BIT(RCC->AHB2ENR, RCC_AHB2ENR_OTGFSEN);
+    
+    // Setup USB GPIO
+    GPIOA->MODER &= ~(0x3U << (PINNO(PIN_USB_DM)*2));
+    GPIOA->MODER |= (0x2U << (PINNO(PIN_USB_DM)*2));
+    GPIOA->OSPEEDR |= (0x3U << (PINNO(PIN_USB_DM)*2));
+    GPIOA->AFR[1] &= ~(0x15U << ((PINNO(PIN_USB_DM) & 7)*4));
+    GPIOA->AFR[1] |= (0xAU << ((PINNO(PIN_USB_DM) & 7)*4));
+    GPIOA->MODER &= (uint32_t)~(uint32_t)((uint32_t)0x3UL << (uint32_t)(PINNO(PIN_USB_DP)*2UL));
+    GPIOA->MODER |= (0x2U << (PINNO(PIN_USB_DP)*2));
+    GPIOA->OSPEEDR |= (0x3U << (PINNO(PIN_USB_DP)*2));
+    GPIOA->AFR[1] &= ~(0x15U << ((PINNO(PIN_USB_DP) & 7)*4));
+    GPIOA->AFR[1] |= (0xAU << ((PINNO(PIN_USB_DP) & 7)*4));
 
-    // Enable VBUS sensing in the GCCFG register
-    USB_OTG_FS->GCCFG |= USB_OTG_GCCFG_VBDEN;
+    // Init the USB AHB Interface
+    // USB_OTG_FS->GAHBCFG = 0x0UL;
+    // Global PHY Interrupt enable
+    USB_OTG_FS->GAHBCFG |= USB_OTG_GAHBCFG_GINT;
+    // Interrupt on Fully empty TX FIFO
+    // USB_OTG_FS->GAHBCFG &= ~USB_OTG_GAHBCFG_TXFELVL;
+    // USB_OTG_FS->GINTSTS &= ~USB_OTG_GINTSTS_RXFLVL;
+    USB_OTG_FS->GCCFG |= USB_OTG_GCCFG_VBDEN;  // Enable VBUS sensing
+    USB_OTG_FS->GCCFG |= USB_OTG_GCCFG_PWRDWN;  // Power up the USB core
 
-    // gpio_write(PIN_USB_VBUS, true);
+    // Init the USB Core
+    // USB_OTG_FS->GUSBCFG = 0x00001440; 
+    USB_OTG_FS->GUSBCFG |= USB_OTG_GUSBCFG_FDMOD | USB_OTG_GUSBCFG_HNPCAP | USB_OTG_GUSBCFG_SRPCAP;
+    USB_OTG_FS->GUSBCFG &= ~(USB_OTG_GUSBCFG_FHMOD);
+    USB_OTG_FS->GUSBCFG |= USB_OTG_GUSBCFG_TRDT_3;
+    USB_OTG_FS->GUSBCFG |= USB_OTG_GUSBCFG_TOCAL_2;
+
+    // Unmask Required Interrupt Bits
+    // USB_OTG_FS->GINTMSK |= USB_OTG_GINTMSK_OTGINT | USB_OTG_GINTMSK_MMISM;
+    // USB_OTG_FS->GINTMSK |= USB_OTG_GINTMSK_OTGINT; //| USB_OTG_GINTMSK_MMISM;
+    
+    bool devmod = !((USB_OTG_FS->GINTSTS & USB_OTG_GINTSTS_CMOD) > 0);
+    printf("Device Mode Enabled = %d\n", devmod);
+
+    
+}
+
+void USB_Init_Device(void){
+    USB_OTG_DeviceTypeDef *usbdev = (USB_OTG_DeviceTypeDef*)USB_OTG_DEVICE_BASE;
+    // Set Periodic Frame interval to 90% | Device Speed to Full Speed using FS PHY
+    usbdev->DCFG |= USB_OTG_DCFG_PFIVL_1 | (0x3 << USB_OTG_DCFG_DSPD_Pos);
+    // Send out packet to the application and send NACK packet to host
+    usbdev->DCFG &= ~(USB_OTG_DCFG_NZLSOHSK);
+    // Clear Soft Disconnect Bit
+    usbdev->DCTL &= ~(USB_OTG_DCTL_SDIS);
+
+    // Enable Interrupt Bits
+    // USB_OTG_HS->GINTMSK |= USB_OTG_GINTMSK_USBRST | USB_OTG_GINTMSK_ENUMDNEM | USB_OTG_GINTMSK_ESUSPM | USB_OTG_GINTMSK_USBSUSPM | USB_OTG_GINTMSK_SOFM;
+    USB_OTG_HS->GINTMSK |= USB_OTG_GINTMSK_USBRST | USB_OTG_GINTMSK_SOFM;
+}
+
+static inline void USB_RemoteWake(void){
+    // printf("Attempting to wake Host\n");
+    USB_OTG_DeviceTypeDef *usbdev = (USB_OTG_DeviceTypeDef*)USB_OTG_DEVICE_BASE;
+    usbdev->DCTL |= USB_OTG_DCTL_RWUSIG;
+    // Wait 2ms and clear bit
+    vTaskDelay(2);
+    usbdev->DCTL &= ~(USB_OTG_DCTL_RWUSIG);
+}
+
+volatile bool reset_detected = false, reset_ended = false;
+
+void vUSB_tsk(void * pvParams){
+    (void)pvParams;
+
+    printf("Initializing USB Core\n");
+    USB_Init_Core();
+    printf("USB Core Initialized\n");
+    printf("Initializing USB Device\n");
+    USB_Init_Device();
+    printf("USB Device Initialized\n");
+    printf("Attempting to wake Host\n");
+    USB_RemoteWake();
+
     for(;;){
-        // Optional: Check for session request (Vbus detection)
-        if (USB_OTG_FS->GINTSTS & USB_OTG_GINTSTS_SRQINT)
-        {
-            printf("Session Request Detected (Vbus Present)\n");
-        }
-        bool vbus_active = USB_OTG_FS->GOTGCTL & USB_OTG_GOTGCTL_BSESVLD;
-        // if(vbus_active) printf("VBUS active: %d\n", vbus_active);
-
-        // bool session_active = USB_OTG_FS->GINTSTS & USB_OTG_GINTSTS_SRQINT;
-        if(session_active) printf("Session Request Interrupt: %d\n", session_active);
-
+        if(reset_detected)
+            printf("USB reset detected\n");
+        else
+            USB_RemoteWake();
+        if(reset_ended)
+            printf("USB reset finished\n");
         vTaskDelay(10);
     }
-
     // Main USB task delay to allow for USB processing
     vTaskDelay(100);
     printf("Deleting USB task\n");
@@ -216,17 +204,18 @@ void vUSB_tsk(void * pvParams){
 // Interrupt handler for USB OTG FS
 void OTG_FS_IRQHandler(void) {
     uint32_t gintsts = USB_OTG_FS->GINTSTS;
-    session_active = USB_OTG_FS->GINTSTS & USB_OTG_GINTSTS_SRQINT;
 
     // Check for USB reset interrupt (used when the USB host resets the device)
     if (gintsts & USB_OTG_GINTSTS_USBRST) {
         // printf("USB Reset Interrupt\n");
+        reset_detected = !reset_detected;
         USB_OTG_FS->GINTSTS = USB_OTG_GINTSTS_USBRST;  // Clear interrupt flag
     }
 
     // Handle enumeration done (when device enumeration is complete)
     if (gintsts & USB_OTG_GINTSTS_ENUMDNE) {
         // printf("USB Enumeration Done\n");
+        reset_ended = !reset_ended;
         USB_OTG_FS->GINTSTS = USB_OTG_GINTSTS_ENUMDNE;  // Clear interrupt flag
     }
 
@@ -243,7 +232,21 @@ void OTG_FS_IRQHandler(void) {
         USB_OTG_FS->GINTSTS = USB_OTG_GINTSTS_SOF;      // Clear interrupt flag
     }
 
+    if(gintsts & USB_OTG_GINTSTS_GINAKEFF){
+        USB_OTG_FS->GINTSTS = USB_OTG_GINTSTS_GINAKEFF;      // Clear interrupt flag
+    }
+
+    if(gintsts & 0x10){
+        USB_OTG_FS->GINTSTS = 0x10;      // Clear interrupt flag
+    }
+
+    if(gintsts & USB_OTG_GINTSTS_OTGINT){
+        uint32_t goint = USB_OTG_HS->GOTGINT;
+        USB_OTG_FS->GINTSTS = USB_OTG_GINTSTS_OTGINT;      // Clear interrupt flag
+    }
     // Handle any other interrupts you need to process
+    // Clear Everthing
+    // USB_OTG_FS->GINTSTS = gintsts;
 }
 
 // Initialize all system Interfaces
