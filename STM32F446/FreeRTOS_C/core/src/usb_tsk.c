d7819ab6b7893f8d0c973e8b7bc70f21f1fae597
Jacob Chisholm <Jacobchisholm1010@gmail.com>
2024-10-19 21:58:09 -0400

    wip

--- STM32F446/FreeRTOS_C/core/src/usb_tsk.c
+++ STM32F446/FreeRTOS_C/core/src/usb_tsk.c
@@ -1,13 +1,3 @@
-/**
- * @file main.c
- * @author Jacob Chisholm (https://jchisholm.github.io) //
- * @brief System Entry Point
- * @date 2023-08-30
- * @version 2.2
- * 
- */
-
-
 #include "main.h"
 #include <stdio.h>
 #include "stm32f446xx.h"
@@ -23,48 +13,6 @@
 #include "os/hal/usb/hal_usbd_otgfs.h"
 #include "os/hal/usb/usb_std.h"
 #include "os/hal/usb/usb_cdc.h"
-
-#define debug_led PIN('D', 12)
-
-
-void vTestTask(void * pvParams){
-    (void)pvParams;
-    char* str = "Hello World from Serial 2\n";
-    gpio_set_mode(PIN('B', 0), GPIO_MODE_OUTPUT);
-    vTaskDelay(100);
-    // hal_uart_init(USART2, 9600, PIN('A', 2), PIN('A', 3));
-    for(;;){
-        gpio_write(PIN('B', 0), !gpio_read_odr(PIN('B', 0)));
-        // serial_write(&Serial2, str, strlen(str), 100);
-        // printf("Hello from Serial 2\n");
-        // hal_uart_write_buf(USART2, (char*)str, strlen(str));
-        // Serial2.write((uint8_t*)str, strlen(str));
-        // sleep for 1000 ms
-        vTaskDelay(1000);
-    }
-}
-
-void vUART_FeedBack(void * pvParams){
-    (void)pvParams;
-    // StreamBufferHandle_t hndl = xStreamBufferCreateStatic(100, 1, NULL, NULL);
-    StreamBufferHandle_t rx_buf = xStreamBufferCreate(100, 5);
-    // StreamBufferHandle_t xStreamBuffer = xStreamBufferCreate(100, 1);
-    // Serial2.attach(xStreamBuffer);
-    serial_attach(&Serial3, &rx_buf);
-    // char * msg = "Serial 2 got: ";
-    vTaskDelay(1000);
-    printf("Uart Feedback online\n");
-    for(;;){
-        char buffer[100] = {0};
-        xStreamBufferReceive(rx_buf, (void*)buffer, 100, portMAX_DELAY);
-        gpio_write(PIN('B', 0), !gpio_read_odr(PIN('B', 0)));
-        // serial_write(&Serial2, msg, strlen(msg), 100);
-        // serial_write(&Serial2, buffer, bytes, 100);
-        printf("Serial 2 RX: %s\n", buffer);
-        // printf("Serial 2 got: %s", buffer);
-        // vTaskDelay(10);
-    }
-}
 
 #define CDC_EP0_SIZE 0x08
 
@@ -297,40 +245,3 @@ void OTG_FS_IRQHandler(void) {
 
     // Clear other necessary interrupt flags if needed
 }
-
-
-// Initialize all system Interfaces
-void Init(void){
-    // Initialize UART
-    serial_init(&Serial3, 9600, PIN_USART3_RX, PIN_USART3_TX);
-    // Required for MockECU Board
-    // gpio_set_mode(PIN('A', 10), GPIO_MODE_OUTPUT);
-    // gpio_write(PIN('A', 10), false);
-    // gpio_set_mode(PIN_LED2, GPIO_MODE_OUTPUT);
-    // Initialize CAN
-    // can_init(&CANBus1, CAN_1000KBPS, PIN_CAN1_RX, PIN_CAN1_TX);
-
-}
-
-
-int main(void){
-
-    Init();
-    
-    // THESE SHOULD BE STATIC
-    xTaskCreate(vTestTask, "TestTask", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
-    xTaskCreate(vUART_FeedBack, "S2 Echo", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
-    xTaskCreate(vUSB_tsk, "USB tst", configMINIMAL_STACK_SIZE<<1, NULL, 1, NULL);
-    // xTaskCreate(vCanTask, "Can1RX", configMINIMAL_STACK_SIZE, NULL, 0, NULL);
-
-    // Start Scheduler
-    vTaskStartScheduler();
-
-    // System Main loop (Should never run -> Scheduler runs infinitely)
-    for(;;) {
-        asm("nop");
-        gpio_write(PIN_LED2, true);
-    }
-    return 0;
-}
-
