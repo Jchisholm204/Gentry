d50b0c8ef7f2791af55fc7b68d227487700b49e0
Jacob Chisholm <jacobchisholm1010@gmail.com>
2024-11-14 18:20:48 -0500

    wip

---
+++ STM32F446/FreeRTOS_C/core/include/os/hal/hal_usbfs.h
@@ -0,0 +1,16 @@
+/**
+ * @file hal_usbfs.h
+ * @author Jacob Chisholm (https://jchisholm.github.io) //
+ * @brief STM32 USB FS HAL
+ * @date 2024-11-14
+ * @version 0.1
+ * 
+ */
+
+#ifndef _HAL_USBFS_H_
+#define _HAL_USBFS_H_
+
+
+
+#endif
+
