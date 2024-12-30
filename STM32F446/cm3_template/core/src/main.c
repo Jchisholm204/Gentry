07a143a6cc4d7d3d20a5e72b8c445fbabd59d418
Jacob Chisholm <jacobchisholm1010@gmail.com>
2024-12-29 19:09:40 -0800

    create proj

---
+++ STM32F446/cm3_template/core/src/main.c
@@ -0,0 +1,59 @@
+/**
+ * @file main.c
+ * @author Jacob Chisholm (https://jchisholm.github.io) //
+ * @brief System Entry Point
+ * @date 2023-08-30
+ * @version 2.2
+ * 
+ */
+
+
+#include <stdio.h>
+#include "FreeRTOS.h"
+#include "os/config/FreeRTOSConfig.h"
+#include "task.h"
+#include "string.h"
+#include "libopencm3/stm32/rcc.h"
+#include "libopencm3/stm32/gpio.h"
+#include "libopencm3/stm32/usart.h"
+
+
+// Initialize all system Interfaces
+void Init(void){
+    // Initialize UART
+    // serial_init(&Serial3, 9600, PIN_USART3_RX, PIN_USART3_TX);
+    // Required for MockECU Board
+    // gpio_set_mode(PIN('A', 10), GPIO_MODE_OUTPUT);
+    // gpio_write(PIN('A', 10), false);
+    // gpio_set_mode(PIN_LED2, GPIO_MODE_OUTPUT);
+    // Initialize CAN
+    // can_init(&CANBus1, CAN_1000KBPS, PIN_CAN1_RX, PIN_CAN1_TX);
+
+
+}
+
+void vTestTask(void * pvParams);
+void vUART_FeedBack(void * pvParams);
+void vUSB_tsk(void * pvParams);
+
+int main(void){
+
+    Init();
+    
+    // THESE SHOULD BE STATIC
+    xTaskCreate(vTestTask, "TestTask", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
+    xTaskCreate(vUART_FeedBack, "S2 Echo", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
+    xTaskCreate(vUSB_tsk, "USB tst", configMINIMAL_STACK_SIZE<<1, NULL, 1, NULL);
+    // xTaskCreate(vCanTask, "Can1RX", configMINIMAL_STACK_SIZE, NULL, 0, NULL);
+
+    // Start Scheduler
+    vTaskStartScheduler();
+
+    // System Main loop (Should never run -> Scheduler runs infinitely)
+    for(;;) {
+        asm("nop");
+        // gpio_write(PIN_LED2, true);
+    }
+    return 0;
+}
+
