063e8ca6d55937a0b74a4a8e623db4c616351698
Jacob Chisholm <jacobchisholm1010@gmail.com>
2025-01-07 09:24:41 -0500

    wip

--- STM32F446/cm3_template/libopencm3.cmake
+++ STM32F446/cm3_template/libopencm3.cmake
@@ -1,24 +1,18 @@
-include(FetchContent)
-# FetchContent_Declare(libopencm3
-#   GIT_REPOSITORY https://github.com/libopencm3/libopencm3
-#   GIT_TAG master
-# )
-# FetchContent_MakeAvailable(libopencm3)
-# FetchContent_GetProperties(libopencm3)
+set(CM3_DIR ${CMAKE_CURRENT_SOURCE_DIR}/lib/libopencm3)
 
 # add_custom_target(libopencm3 make WORKING_DIRECTORY ${libopencm3_SOURCE_DIR})
 # create a target to build libopencm3 -- only for the target we need
 add_custom_target(libopencm3 
     make
     TARGETS=stm32/f4 
-    WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/lib/libopencm3)
+    WORKING_DIRECTORY ${CM3_DIR})
 
 # Create a specific CPU target with the appropriate options etc
 add_library(stm32f446 STATIC IMPORTED)
-set_property(TARGET stm32f446 PROPERTY INTERFACE_INCLUDE_DIRECTORIES ${libopencm3_SOURCE_DIR}/include)
-set_property(TARGET stm32f446 PROPERTY IMPORTED_LOCATION ${libopencm3_SOURCE_DIR}/lib/libopencm3_stm32f4.a)
+set_property(TARGET stm32f446 PROPERTY INTERFACE_INCLUDE_DIRECTORIES ${CM3_DIR}/include)
+set_property(TARGET stm32f446 PROPERTY IMPORTED_LOCATION ${CM3_DIR}/lib/libopencm3_stm32f4.a)
 add_dependencies(stm32f446 libopencm3)
-target_link_directories(stm32f446 INTERFACE ${libopencm3_SOURCE_DIR}/lib)
+target_link_directories(stm32f446 INTERFACE ${CM3_DIR}/lib)
 
 target_compile_definitions(stm32f446 INTERFACE -DSTM32F4)
 
