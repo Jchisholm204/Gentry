358c48d2606f53d572847dbb15d8ae4d12230fb6
Jacob Chisholm <jacobchisholm1010@gmail.com>
2025-02-18 23:15:18 -0500

    gripper test

--- QSET/ARM_V1/driver/test/main.c
+++ QSET/ARM_V1/driver/test/main.c
@@ -3,6 +3,7 @@
 #include <stdbool.h>
 #include <memory.h>
 #include <libusb-1.0/libusb.h>
+#include <ncurses.h>
 
 
 armDev_t dev;
@@ -21,7 +22,10 @@ int main(void){
     printf("udev_connected\n");
     bool dis = false;
     int8_t gval = -128;
+    initscr();
     for(;;){
+        char ch = getch();
+        // printf("%c\n", ch);
         if (dev.err != 0) {
             printf("Disconnection Event Detected\n");
             // Device is disconnected, attempt to reconnect
@@ -29,16 +33,30 @@ int main(void){
         }
         else{
             armDev_setMtr(&dev, eJoint1, &mtr);
-            armDev_setGripper(&dev, gval);
+            if(ch == 's'){
+                armDev_setGripper(&dev, 0);
+                printf("Stopping Gripper");
+            }
+            else if(ch == 'j'){
+                armDev_setGripper(&dev, 127);
+                printf("Opening Gripper");
+            }
+            else if(ch == 'k'){
+                armDev_setGripper(&dev, -127);
+                printf("Closing Gripper");
+            }
+            else{
+                armDev_setGripper(&dev, 0);
+            }
             mtr.enable = 0;
             // armDev_setServo(&dev, eServo2, 2200);
             // armDev_setGripper(&dev, 120);
             struct udev_pkt_status *sts = armDev_getStatusPkt(&dev);
-            printf("Joint1: %0.2f rad/s LS: %d\n", sts->mtr[eJoint1].position, sts->limit_sw & 0x7F);
+            // printf("Joint1: %0.2f rad/s LS: %d\n", sts->mtr[eJoint1].position, sts->limit_sw & 0x7F);
         }
         // sleep(1);
-        gval += 1;
-        if(gval > 120) gval = -120;
+        // gval += 1;
+        // if(gval > 120) gval = -120;
     }
     armDev_free(&dev);
     return 0;
