f3ac1e911399207c7508f864977f77c3321ee575
Jacob Chisholm <jacobchisholm1010@gmail.com>
2025-04-09 08:22:53 -0400

    docs

--- QSET/PAYLOAD_V1/core/include/motor_ctrl.h
+++ QSET/PAYLOAD_V1/core/include/motor_ctrl.h
@@ -1,8 +1,8 @@
 /**
- * @file srv_ctrl.h
+ * @file motor_ctrl.h
  * @author Jacob Chisholm (https://jchisholm.github.io)
- * @brief QSET Servo Control - Payload Control
- * @date 2025-04-05
+ * @brief QSET Motor Control - Payload Control
+ * @date 2025-04-09
  * @version 1.0
  * 
  */
@@ -12,9 +12,11 @@
 #include "usb_payload_defs.h"
 #include "pin_cfg.h"
 
+// int8_t maximum value (unsigned)
 #define MTR_MAX (127)
 
 static void motorCtrl_init(void){
+    // Timer 1 Init
     hal_timac_pwm_init(TIM1, (PLL_N/PLL_P)-1, MTR_MAX-1);
     hal_timac_pwm_configure_channel(TIM1, eTimCh1);
     hal_timac_pwm_configure_nChannel(TIM1, eTimCh1);
@@ -29,7 +31,7 @@ static void motorCtrl_init(void){
     gpio_set_mode(PIN_M21N, GPIO_MODE_AF);
     gpio_set_af(PIN_M21N, GPIO_AF_TIM1);
 
-    
+    // Timer 8 Init
     hal_timac_pwm_init(TIM8, (PLL_N/PLL_P)-1, MTR_MAX-1);
     hal_timac_pwm_configure_channel(TIM8, eTimCh1);
     hal_timac_pwm_configure_nChannel(TIM8, eTimCh1);
@@ -57,17 +59,21 @@ static void motorCtrl_init(void){
 
 static inline void motorCtrl_set(enum ePayloadMotor motor, int8_t speed){
     // H-Bridge Speed Logic - PWM high side, Ground low side
+    // High and low variables refer to the +/- labeling on the board
+    // If the code labels are swapped, the motor will run in reverse
     uint32_t high = 0, low = 0;
     if(speed >= 0){
+        // Pulse the high side between Voltage/Ground
         high = (uint32_t)((uint8_t)(speed) & 0xFF);
+        // Pin the low side to ground
         low  = 127;
     }
     else{
+        // Pin the high side to ground
         high = 0;
+        // Pulse the low side between Voltage/Ground
         low  = (uint32_t)((uint8_t)(127 + speed) & 0xFF);
     }
-    // high = (uint32_t)((uint8_t)(127 - speed) & 0xFF);
-    // low  = (uint32_t)((uint8_t)(127 + speed) & 0xFF);
 
     // Switch for the different motors
     switch(motor){
